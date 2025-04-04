75b8de7cee4c7ff872d84eea195aff2431b76942
mukunthkannan <mukunthkannan1204@gmail.com>
2025-04-04 15:18:39 +0530

    Add files via upload

---
+++ myjavascript.js
@@ -0,0 +1,8 @@
+
+        function changebackground() {
+            const colors =["blue", "green", "pink", "yellow", "red", "purple"];
+            document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+            alert("Hello my name is mukunth. My age is 21");
+            
+        }
+       
